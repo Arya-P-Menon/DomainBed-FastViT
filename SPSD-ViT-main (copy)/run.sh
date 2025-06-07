12092a2fd8d1c4b03574e16d69630f04e48e6b64
Arya-P-Menon <81412152+Arya-P-Menon@users.noreply.github.com>
2025-06-07 16:14:38 +0000

    Update run.sh

--- SPSD-ViT-main (copy)/run.sh
+++ SPSD-ViT-main (copy)/run.sh
@@ -1,7 +1,17 @@
+#for command in delete_incomplete launch
+#do
+#  python -m domainbed.scripts.sweep ${command} --data_dir=./data \
+#  --output_dir=./output --command_launcher "multi_gpu" --algorithms ERM_SPSDViT \
+#  --single_test_envs  --datasets DR --n_hparams 1 --n_trials 3  \
+#  --hparams """{\"backbone\":\"CVTSmall\",\"batch_size\":16,\"lr\":5e-05,\"resnet_dropout\":0.0,\"weight_decay\":0.0}"""
+#done
+
+
 for command in delete_incomplete launch
 do
-  python -m domainbed.scripts.sweep ${command} --data_dir=./data \
-  --output_dir=./output --command_launcher "multi_gpu" --algorithms ERM_SPSDViT \
-  --single_test_envs  --datasets DR --n_hparams 1 --n_trials 3  \
+  python -m domainbed.scripts.sweep_updated ${command} --data_dir=./data \
+  --output_dir=./output_SDG --command_launcher "multi_gpu" --algorithms ERM_SPSDViT \
+  --single_domain_generalization  --datasets DR --n_hparams 1 --n_trials 3  \
   --hparams """{\"backbone\":\"CVTSmall\",\"batch_size\":16,\"lr\":5e-05,\"resnet_dropout\":0.0,\"weight_decay\":0.0}"""
 done
+
